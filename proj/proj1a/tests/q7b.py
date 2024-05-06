4ea55de8360bc7e3a39f1fc141f36b6d83a1fdab
StellarLane <152025819+StellarLane@users.noreply.github.com>
2024-05-06 15:25:32 +0800

    project 1 done (except Q8)
    fixed a typo in the commit message😂

--- proj/proj1a/tests/q7b.py
+++ proj/proj1a/tests/q7b.py
@@ -1,6 +1,7 @@
+OK_FORMAT=True
 test = {   'name': 'q7b',
     'points': 2,
-    'suites': [   {   'cases': [   {   'code': ">>> expected_ohe_cols = set(['x0_Other', 'x0_Shake', 'x0_Shingle/Asphalt' ,'x0_Slate', 'x0_Tar&Gravel', 'x0_Tile'])\n"
+    'suites': [   {   'cases': [   {   'code': ">>> expected_ohe_cols = set(['Roof Material_Other', 'Roof Material_Shake', 'Roof Material_Shingle/Asphalt' ,'Roof Material_Slate', 'Roof Material_Tar&Gravel', 'Roof Material_Tile'])\n"
                                                '>>> expected_ohe_cols.issubset(set(training_data.columns)) == True\n'
                                                'True',
                                        'hidden': False,
